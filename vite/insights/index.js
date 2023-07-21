0932043cd465afb895add96b358d5abee7cb1be5
Miško Hevery <misko@hevery.com>
2023-07-21 16:51:43 +0000

    4000c75 feat(insights): add icons and dashboard. (#4803)
    https://github.com/BuilderIO/qwik/commit/2fd41d9208f61822cb50322d499eb2b90c7db7bf

--- vite/insights/index.js
+++ vite/insights/index.js
@@ -3,10 +3,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.insightsEntryStrategy = void 0;
 async function insightsEntryStrategy({ publicApiKey, }) {
     const request = await fetch(`https://qwik-insights.builder.io/api/v1/${publicApiKey}/bundles/`);
-    const bundles = await request.json();
-    return {
-        type: 'smart',
-        manual: bundles,
-    };
+    try {
+        const bundles = await request.json();
+        return {
+            type: 'smart',
+            manual: bundles,
+        };
+    }
+    catch (e) {
+        console.error(e);
+        return {
+            type: 'smart',
+        };
+    }
 }
 exports.insightsEntryStrategy = insightsEntryStrategy;
