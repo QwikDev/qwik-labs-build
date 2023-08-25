b9bfcc603b833c1e8e8b059e8f43b5311a257c49
Giorgio Boa <35845425+gioboa@users.noreply.github.com>
2023-08-25 16:15:43 +0000

    2c560f9 insights(feat): qwikInsights Vite Plugin + save symbol details (#5011)
    https://github.com/BuilderIO/qwik/commit/c777573282782088779bb29757ae0d4f1819ab60

--- vite/insights/index.js
+++ vite/insights/index.js
@@ -1,20 +1,49 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.insightsEntryStrategy = void 0;
-async function insightsEntryStrategy({ publicApiKey, }) {
-    const request = await fetch(`https://qwik-insights.builder.io/api/v1/${publicApiKey}/bundles/`);
-    try {
-        const bundles = await request.json();
-        return {
-            type: 'smart',
-            manual: bundles,
-        };
-    }
-    catch (e) {
-        console.error(e);
-        return {
-            type: 'smart',
-        };
-    }
+exports.qwikInsights = void 0;
+const fs_1 = require("fs");
+const promises_1 = require("fs/promises");
+const INSIGHTS_Q_MANIFEST_FILENAME = './dist/q-insights.json';
+const logWarn = (message) => {
+    console.warn('\x1b[33m%s\x1b[0m', `\n\nQWIK WARN: ${message}\n`);
+};
+async function qwikInsights(qwikInsightsOpts) {
+    const { publicApiKey, baseUrl = 'https://qwik-insights.builder.io' } = qwikInsightsOpts;
+    let isProd = false;
+    const vitePlugin = {
+        name: 'vite-plugin-qwik-insights',
+        enforce: 'pre',
+        async config(viteConfig) {
+            isProd = viteConfig.mode !== 'ssr';
+            if (isProd) {
+                const qManifest = { type: 'smart' };
+                try {
+                    const response = await fetch(`${baseUrl}/api/v1/${publicApiKey}/bundles/`);
+                    const bundles = await response.json();
+                    qManifest.manual = bundles;
+                }
+                catch (e) {
+                    logWarn('fail to fetch manifest from Insights DB');
+                }
+                await (0, promises_1.writeFile)(INSIGHTS_Q_MANIFEST_FILENAME, JSON.stringify(qManifest));
+            }
+        },
+        closeBundle: async () => {
+            const Q_MANIFEST_FILENAME = './dist/q-manifest.json';
+            if (isProd && (0, fs_1.existsSync)('./dist/q-manifest.json')) {
+                const qManifest = await (0, promises_1.readFile)(Q_MANIFEST_FILENAME, 'utf-8');
+                try {
+                    await fetch(`${baseUrl}/api/v1/${publicApiKey}/post/manifest`, {
+                        method: 'post',
+                        body: qManifest,
+                    });
+                }
+                catch (e) {
+                    logWarn('fail to post manifest to Insights DB');
+                }
+            }
+        },
+    };
+    return vitePlugin;
 }
-exports.insightsEntryStrategy = insightsEntryStrategy;
+exports.qwikInsights = qwikInsights;
