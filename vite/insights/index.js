a487845dd5039e3e178f69755f603cbc2edef2c2
Giorgio Boa <35845425+gioboa@users.noreply.github.com>
2023-08-29 23:55:34 +0000

    d27177d fix: qwikInsights build (#5051)
    https://github.com/BuilderIO/qwik/commit/e6fa76c8cd10f5b1018a86a9baf900da5b982e76

--- vite/insights/index.js
+++ vite/insights/index.js
@@ -3,13 +3,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.qwikInsights = void 0;
 const fs_1 = require("fs");
 const promises_1 = require("fs/promises");
-const INSIGHTS_Q_MANIFEST_FILENAME = './dist/q-insights.json';
+const node_path_1 = require("node:path");
 const logWarn = (message) => {
     console.warn('\x1b[33m%s\x1b[0m', `\n\nQWIK WARN: ${message}\n`);
 };
 async function qwikInsights(qwikInsightsOpts) {
     const { publicApiKey, baseUrl = 'https://qwik-insights.builder.io' } = qwikInsightsOpts;
     let isProd = false;
+    const outDir = 'dist';
     const vitePlugin = {
         name: 'vite-plugin-qwik-insights',
         enforce: 'pre',
@@ -25,13 +26,16 @@ async function qwikInsights(qwikInsightsOpts) {
                 catch (e) {
                     logWarn('fail to fetch manifest from Insights DB');
                 }
-                await (0, promises_1.writeFile)(INSIGHTS_Q_MANIFEST_FILENAME, JSON.stringify(qManifest));
+                if (!(0, fs_1.existsSync)((0, node_path_1.join)(process.cwd(), outDir))) {
+                    (0, fs_1.mkdirSync)((0, node_path_1.join)(process.cwd(), outDir));
+                }
+                await (0, promises_1.writeFile)((0, node_path_1.join)(process.cwd(), outDir, 'q-insights.json'), JSON.stringify(qManifest));
             }
         },
         closeBundle: async () => {
-            const Q_MANIFEST_FILENAME = './dist/q-manifest.json';
-            if (isProd && (0, fs_1.existsSync)('./dist/q-manifest.json')) {
-                const qManifest = await (0, promises_1.readFile)(Q_MANIFEST_FILENAME, 'utf-8');
+            const path = (0, node_path_1.join)(process.cwd(), outDir, 'q-manifest.json');
+            if (isProd && (0, fs_1.existsSync)(path)) {
+                const qManifest = await (0, promises_1.readFile)(path, 'utf-8');
                 try {
                     await fetch(`${baseUrl}/api/v1/${publicApiKey}/post/manifest`, {
                         method: 'post',
