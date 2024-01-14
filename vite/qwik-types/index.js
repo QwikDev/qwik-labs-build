b12ce2b83c755ac5781313d840dae60620875d5e
Miško Hevery <misko@hevery.com>
2024-01-14 06:20:41 +0000

    8e1887b fix: prevent race condition
    https://github.com/BuilderIO/qwik/commit/c2e652875347b4aea5e98219da17ef629b102795

---
+++ vite/qwik-types/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.qwikTypes = void 0;
+var vite_1 = require("./vite");
+Object.defineProperty(exports, "qwikTypes", { enumerable: true, get: function () { return vite_1.qwikTypes; } });
