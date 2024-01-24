61a27f52c322b26ac4cd590c63d727e5f9fbdb3a
thejackshelton <me@jackshelton.com>
2024-01-24 01:48:53 +0000

    048a7ba feat(vite): new file filter option
    https://github.com/BuilderIO/qwik/commit/a5816403a5e3d2cec46fe5b4b7be5adeccefe690

---
+++ vite/insights/index.d.ts
@@ -0,0 +1,6 @@
+import { type PluginOption } from 'vite';
+export declare function qwikInsights(qwikInsightsOpts: {
+    publicApiKey: string;
+    baseUrl?: string;
+    outDir?: string;
+}): Promise<PluginOption>;
