f89ec517a96bc540bfbeda23af6168ffd7f6f3c6
Varixo <admin@varixo.pl>
2024-09-26 21:20:07 +0000

    3205a3a move use hooks to separate files
    https://github.com/QwikDev/qwik/commit/e3c12a902249c2d2a72a7509aa0fab4a0e57013f

---
+++ src/root.tsx
@@ -0,0 +1,12 @@
+export default () => {
+  return (
+    <>
+      <head>
+        <meta charset="utf-8" />
+        <title>Qwik Blank App</title>
+        <script type="module" src="/src/debug/index.ts" />
+      </head>
+      <body>Qwik Labs</body>
+    </>
+  );
+};
